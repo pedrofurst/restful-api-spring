c7a1549dfa4730cf80d2ea5d0fdb085dec269b9f
Pedro Furst <pedrofurst@gmail.com>
2017-08-04 13:14:46 -0300

    fix

--- bin/data-h2.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO USERS (id, name, email) VALUES (1, 'Batman', 'batman@batmail.com');
-INSERT INTO USERS (id, name, email) VALUES (2, 'Patrick Star', 'patrick@star.com');
-INSERT INTO USERS (id, name, email) VALUES (3, 'Sponge Bob', 'sponge@bob.com');
