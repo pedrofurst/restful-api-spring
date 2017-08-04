4b07625166e8e9ee4c0c54e969c8bfe562a15b6d
Pedro Furst <pedrofurst@uppoints.com>
2017-08-04 17:13:53 -0300

    fixes

--- src/main/java/com/uppoints/service/UserServiceImpl.java
+++ src/main/java/com/uppoints/service/UserServiceImpl.java
@@ -2,16 +2,17 @@ package com.uppoints.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uppoints.model.User;
 import com.uppoints.repository.UserRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	@Autowired
 	private UserRepository repository;
 
 	@Override
